4f80c89fa416ecb6d5b4747fdaad18889bf7d2ef
Julio Santana <julio.python777@gmail.com>
2023-05-23 09:37:08 -0300

    criando html do formulario e categorias

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const connection = require("./database/database");
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/articlesController");
+const Article = require("./articles/Article");
+const Category = require("./categories/Category");
 //View ewngine
 app.set("view engine", "ejs");
 //Static
