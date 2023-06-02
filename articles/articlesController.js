658f6da63015483608216aa4029c1da440980304
Julio Santana <julio.python777@gmail.com>
2023-06-02 18:21:44 -0300

    criando logica de paginação

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -74,4 +74,61 @@ router.get("/admin/articles/edit/:id", (req, res) => {
       res.redirect("/");
     });
 });
+
+router.post("/articles/update", (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.update(
+    {
+      title: title,
+      body: body,
+      categoryId: category,
+      slug: slugify(title),
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  )
+    .then(() => {
+      res.redirect("/admin/article");
+    })
+    .catch((err) => {
+      res.redirect("/");
+    });
+});
+
+router.get("/articles/page/:num", (req, res) => {
+  var page = req.params.num;
+  var offset = 0;
+
+  if (isNaN(page) || page == 1) {
+    offset = 0;
+  } else {
+    offset = parseInt(page) * 4;
+  }
+
+  Article.findAndCountAll({
+    limit: 4,
+    offset: offset,
+  }).then((articles) => {
+    var next;
+    if (offset + 4 >= articles.count) {
+      next = false;
+    } else {
+      next = true;
+    }
+
+    var result = {
+      next: next,
+      articles: articles,
+    };
+
+    res.json(result);
+  });
+});
 module.exports = router;
