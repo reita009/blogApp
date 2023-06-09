c538c181fd9d3abccaa244304bfc0b1f2f89f0ae
Julio Santana <julio.python777@gmail.com>
2023-06-09 14:14:09 -0300

    autenticação de email e senha

--- users/UsersController.js
+++ users/UsersController.js
@@ -42,4 +42,31 @@ router.post("/users/create", (req, res) => {
   });
 });
 
+router.get("/login", (req, res) => {
+  res.render("admin/users/login");
+});
+
+router.post("/authenticate", (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  User.findOne({ where: { email: email } }).then((user) => {
+    if (user != undefined) {
+      //se existir o usuario com email vamos validar senha
+      var correct = bcrypt.compareSync(password, user.password);
+      if (correct) {
+        req.session.user = {
+          id: user.id,
+          email: user.email,
+        };
+        res.json(req.session.user);
+      } else {
+        res.redirect("login");
+      }
+    } else {
+      res.redirect("/login");
+    }
+  });
+});
+
 module.exports = router;
